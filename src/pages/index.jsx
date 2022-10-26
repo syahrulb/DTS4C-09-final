366d006842b070d05bf71bae48cf6f2391daa833
JKT-LTP-0117 <steven.setiawan@polytron.co.id>
2022-10-26 22:58:18 +0700

    Updates

--- src/pages/index.jsx
+++
@@ -1,4 +0,0 @@
-export { default as Portal } from './Portal'
-export { default as Detail } from './Detail'
-export { default as Login } from './Auth/Login'
-export { default as Register } from './Auth/Register'
--- src/utils/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-const api = axios.create({
-  baseURL: 'http://api.mediastack.com/v1/news'
-})
-export const apiKey = {
-  access_key: process.env.REACT_APP_MEDIASTACK_APIKEY
-}
-export default api
